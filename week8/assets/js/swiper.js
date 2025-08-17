aaa8cc9f545108a42cab5b82b1a8fac40d187b5a
yaj55billy <yaj55ushydna@gmail.com>
2025-08-17 14:12:57 +0800

    feat: 地點搜尋頁 finish

--- week8/assets/js/swiper.js
+++ week8/assets/js/swiper.js
@@ -28,7 +28,51 @@ const swiperSearchResultsTop10 = new Swiper(".swiper-search-results-top10", {
 
 	// Navigation arrows
 	navigation: {
-		nextEl: ".swiper-custom-next",
-		prevEl: ".swiper-custom-prev",
+		nextEl: ".swiper-custom-next-fortop10",
+		prevEl: ".swiper-custom-prev-fortop10",
+	},
+});
+
+const swiperSearchResultsFeature = new Swiper(
+	".swiper-search-results-feature",
+	{
+		// Optional parameters
+		direction: "horizontal",
+		modules: [Navigation, Pagination],
+		spaceBetween: 24,
+		loop: true,
+
+		breakpoints: {
+			320: {
+				slidesPerView: 1.2,
+			},
+			768: {
+				slidesPerView: 2,
+			},
+			1200: {
+				slidesPerView: 3,
+			},
+			1400: {
+				slidesPerView: 4,
+			},
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: ".swiper-custom-next-forfeature",
+			prevEl: ".swiper-custom-prev-forfeature",
+		},
+	}
+);
+
+const swiperSearchResultsHero = new Swiper(".swiper-search-results-hero", {
+	// Optional parameters
+	direction: "horizontal",
+	modules: [Navigation, Pagination],
+	slidesPerView: 1,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination-hero",
+		clickable: true,
 	},
 });
